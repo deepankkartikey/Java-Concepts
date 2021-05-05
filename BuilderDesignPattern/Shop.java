376c6a64ec31e75adb31895fda11521ca52e6278
Deepank kartikey <deepankkartikey@gmail.com>
2021-05-05 11:46:16 +0530

    Builder Design Pattern

---
+++ BuilderDesignPattern/Shop.java
@@ -0,0 +1,8 @@
+package BuilderDesignPattern;
+
+public class Shop {
+    public static void main(String[] args) {
+        Phone p = new PhoneBuilder().setOs("Android").setBattery(6000).getPhone();
+        System.out.println(p);
+    }
+}
